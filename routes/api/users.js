7803d6874df2e0b82c0ff3f7549ae899d2cd8085
GuruRK <gururamkoushik@gmail.com>
2018-11-06 00:10:27 +0530

    email Login implemented with bcrypt/hashing

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const gravatar = require ('gravatar');
-const bcrypt = require ('bcryptjs');
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // Load user model
 const User = require("../../models/User");
@@ -19,30 +19,56 @@ router.post("/register", (req, res) => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
-      const avatar = gravatar.url(req.body.email,{
-          s: '200', // Size
-          r: 'pg',  // Rating
-          d : 'mm'  // Default
-      });  
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // Size
+        r: "pg", // Rating
+        d: "mm" // Default
+      });
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
         avatar: avatar,
         password: req.body.password
       });
-        // hashing the password
-        //sending newUser password to hash
-      bcrypt.genSalt(10, (err, salt)=> {
-          bcrypt.hash(newUser.password, salt, (err, hash) =>{
-               if(err) throw (err);
-              newUser.password = hash;
-              newUser.save()
-               .then(User => res.json(User))
-               .catch(err => console.log(err))
-
-          })
-      })
+      // hashing the password
+      //sending newUser password to hash
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
     }
+  });
+});
+
+//Route - Get api/users/register
+//desc  - Register users route
+//access- Public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //finding user by email
+  User.findOne({ email }).then(user => {
+    // check user
+    if (!user) {
+      return res.status(404).json({ email: "user not found" });
+    }
+
+    // check for password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+          return res.status(400).json({password : 'password incorrect'})
+      }
+    });
   });
 });
 
