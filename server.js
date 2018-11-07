f511987ef805a74d1862d9339ca24c81a278a7fc
GuruRK <gururamkoushik@gmail.com>
2018-11-07 12:10:08 +0530

    passport-Jwt startegy Authentication

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require ('mongoose')
 const bodyParser = require ('body-parser')
+const passport = require ('passport');
 
 const app = express ();
 
@@ -22,9 +23,15 @@ mongoose
     .then(()=> console.log('mongoDB Connected'))
     .catch(err => console.log(err))
 
-app.get ('/', (req, res) => res.send("hello world"));
+// app.get ('/', (req, res) => res.send("hello world"));
 
-//use routes
+//Passport middleware
+app.use(passport.initialize());
+//Passport config
+require('./config/passport')(passport)
+
+
+// routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
